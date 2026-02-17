5358c238354485c31fc6386627573237bc64ca5a
Kylle Raphael Y. Sunga <kylleraphaelsunga@gmail.com>
2026-02-17 19:14:40 +0800

    Update footer links and maintain script functionality

--- includes/footer.php
+++ includes/footer.php
@@ -4,9 +4,9 @@
     </div>
     <div class="footer-right">
         <nav class="footer-nav">
-            <a href="contact.php">Contact</a>
+            <a href="#">Contact</a>
             <a href="about.php">About Us</a>
-            <a href="privacy.php">Privacy</a>
+            <a href="#">Privacy</a>
         </nav>
         <p class="copyright">&copy; <?php echo date("Y"); ?> ImmuniTrack - WD203</p>
     </div>
