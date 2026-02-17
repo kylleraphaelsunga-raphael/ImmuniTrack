483cb2a22b097aeb8d281a87774755f5d39ea850
Kylle Raphael Y. Sunga <kylleraphaelsunga@gmail.com>
2026-02-17 18:49:34 +0800

    Add cache-busting to CSS link in index.php
    Updated the stylesheet link to include a cache-busting query parameter.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ImmuniTrack</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;500;700&display=swap" rel="stylesheet">
     <link rel="icon" type="image/png" href="images/favicon.png">
 </head>
