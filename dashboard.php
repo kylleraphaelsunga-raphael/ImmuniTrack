b1aa53d002986fc13d7fcd100673455d5f68a16e
Kylle Raphael Y. Sunga <kylleraphaelsunga@gmail.com>
2026-02-17 19:04:12 +0800

    Update dashboard content with health topics

--- dashboard.php
+++ dashboard.php
@@ -5,14 +5,16 @@
 
 <main class="dashboard-body">
     <div class="parent">
-        <div class="div1 card"><h3>IMAGE</h3><p>1</p></div>
-        <div class="div2 card"><h3>IMAGE</h3><p>2</p></div>
-        <div class="div3 card"><h3>IMAGE</h3><p>3</p></div>
-        <div class="div4 card"><h3>IMAGE</h3><p>4</p></div>
-        <div class="div5 card"><h3>About NAV 2</h3><p>5</p></div>
-        <div class="div6 card"><h3>About NAV 3</h3><p>6</p></div>
-        <div class="div7 card"><h3>About NAV 4</h3><p>7</p></div>
+        <div class="div1 card"><h3>IMAGE</h3><p>Covid 19</p></div>
+        <div class="div2 card"><h3>IMAGE</h3><p>Influenza</p></div>
+        <div class="div3 card"><h3>IMAGE</h3><p>Chickenpox</p></div>
+        <div class="div4 card"><h3>IMAGE</h3><p>Dengue</p></div>
+        <div class="div5 card"><h3>IMAGE</h3><p>Anti Rabies</p></div>
+        <div class="div6 card"><h3>Information</h3><p>Health Information</p></div>
+        <div class="div7 card"><h3>Healthy Living</h3><p>----</p></div>
+        <div class="div8 card"><h3>Facts or Trivia</h3><p>----</p></div>
     </div>
 </main>
+
 
 <?php include 'includes/footer.php'; ?>
